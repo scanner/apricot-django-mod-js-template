81bf0deab4742ea3feb54f38ddf0db755d6a2d5c
Scanner <scanner@apricot.com>
2013-08-02 17:34:19 -0700

    Begin shapping this to be more like the template I want to use for my django / modular js projects. No license was originally specified so I added the MIT license (hopefully the original author considers that kosher.)

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-An example project based on [django-skel](django-skel.readthedocs.org/en/latest/).
+A Django Project template based on the example project: [django-and-modular-js](http://integricho.github.io/2013/04/10/django-and-modular-js/)
 
-Read the full article describing the power-ups at my ???:
-[django-and-modular-js](http://integricho.github.io/2013/04/10/django-and-modular-js/)
+This is for django projects that are primarily js client app driven with the javascript and css being broken up in a module fashion between various apps. RequireJS is used to make sure all the right pages get the right stuff.
+
+This is also investigating the philosophy that the content templates are primarily being handled by the javascript code, not the django templating engine.
+
+The django templating engine is still used but mostly for meta information holding the structure of the templates.
+
+Well, we shall see. This is an experiment based on a combination of other peolples' ideas to suit the way I want to do things.
+
+
+
--- build.js
+++ build.js
@@ -1,28 +1,33 @@
 {
-    appDir: "./djmodjs/assets/",
-    baseUrl: ".",
-    mainConfigFile: "./djmodjs/assets/common.js",
-    dir: "./djmodjs/assets-built",
-    modules: [
-        {
-            name: "common"
-        },
-        {
-            name: "shared/js/utils",
-            exclude: ["common"]
-        },
-        {
-            name: "treasuremap/js/islands",
-            exclude: ["common"]
-        },
-        {
-            name: "treasuremap/js/map",
-            exclude: ["common"]
-        }
-    ],
-    paths: {
-        "jquery": "empty:",
-        "knockout": "empty:"
+  appDir: "./djmodjs/assets/",
+  baseUrl: ".",
+  mainConfigFile: "./djmodjs/assets/common.js",
+  dir: "./djmodjs/assets-built",
+  modules: [
+    {
+      name: "common"
     },
-    optimizeCss: "standard"
+    {
+      name: "shared/js/utils",
+      exclude: ["common"]
+    },
+
+    // Every one of your js modules needs to be listed here. These are two
+    // from the original example skeleton. ie: islands is 'islands.js' and
+    // 'map' is map.js in the 'djmodjs/assets/js/treasuremap/js/' directory.
+    //
+    // {
+    //     name: "treasuremap/js/islands",
+    //     exclude: ["common"]
+    // },
+    // {
+    //     name: "treasuremap/js/map",
+    //     exclude: ["common"]
+    // }
+  ],
+  paths: {
+    "jquery": "empty:",
+    "knockout": "empty:"
+  },
+  optimizeCss: "standard"
 }
