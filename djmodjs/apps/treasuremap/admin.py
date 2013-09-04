27825b2bb73b4936a7c8b812f33a24f682e45d44
Scanner <scanner@apricot.com>
2013-09-04 16:55:46 -0700

    Remove parts specific to the djmodjs example to tailor it for our use.
    Signed-off-by: Scanner <scanner@apricot.com>

--- djmodjs/apps/treasuremap/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-
-from treasuremap.models import Island, Treasure
-
-
-admin.site.register(Island)
-admin.site.register(Treasure)
--- djmodjs/urls.py
+++ djmodjs/urls.py
@@ -9,7 +9,6 @@ admin.autodiscover()
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
     # Admin panel and documentation:
-    url(r'', include('treasuremap.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
